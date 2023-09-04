c889ea05c13782ae8ca1ac66d5976cbc78f065cb
991 <72177472+Ruan991@users.noreply.github.com>
2023-09-04 12:34:24 -0300

    Add files via upload

--- docs/js/server.js
+++ docs/js/server.js
@@ -2,5 +2,5 @@ function download1() {
     window.open("https://drive.google.com/uc?export=download&id=15Zi9VkqtpSAhDR08oa-wFjb8TlX0TcGP", "_blank"); 
 }
 function download2() {
-    window.open("https://s2.rip", "_blank"); 
+    window.open("https://drive.google.com/uc?export=download&id=1WON0y5KtAc0QQz54AFhgiP5PFv3J-jhk", "_blank"); 
 }
